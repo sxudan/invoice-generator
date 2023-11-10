c52b261dcd8c3e29cc114026e3572185b76679df
Elenaperd24 <elena24.perdomo@gmail.com>
2023-11-10 00:46:04 -0300

    link to invoice-create

--- src/pages/landing_pages.tsx
+++ src/pages/landing_pages.tsx
@@ -6,7 +6,7 @@ import backgroundImage from '../images/landing_page/backgroun_image.jpg'
 function landing_pages() {
     return (
 
-        <div className='sm:absolute top-0 z-1'>
+        <div className='sm:absolute top-0 z-1 h-fit	'>
             <div className='relative flex-col sm:flex sm:flex-row-reverse'>
                 <img className='w-60 m-auto sm:w-1/2  sm:m-0' src={backgroundImage} />
                 <div className='relative flex-col text-center sm:text-left sm:top-24 md:top-40 sm:left-6'>
@@ -14,7 +14,9 @@ function landing_pages() {
                     <br />
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia vitae soluta eveniet amet molestias .</p>
                     <br />
+                    <Link to='/generate-invoice'>
                     <button className='py-2.5 px-3.5 w-48 rounded-xl bg-secondary-color text-primary-font-color'>Create Invoice Now</button>
+                    </Link>
                     <br />
                     <br/>
                     <div className='flex justify-center sm:flex sm:justify-start'>
